b64f9f5975df167928641ed999dcca211c4096df
unknown <1527970101@qq.com>
2025-11-28 19:01:59 +0800

    feat: 初始化项目结构和基础配置

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="page">
+    <div class="title">TODO lIST</div>
+    <div class="container">
+      <div class="top">
+        <div class="addButton">
+          <el-icon><Plus /></el-icon>
+        </div>
+        <div class="search">
+          <div class="searchInput">
+            <input type="text" placeholder="请输入关键字" />
+          </div>
+          <button class="searchButton">搜索</button>
+        </div>
+      </div>
+      <div class="todoForm"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+  font-size: 50px;
+  color: greenyellow;
+}
+.container {
+  width: 500px;
+  height: 100%;
+  margin: 0 auto;
+  background-color: beige;
+}
+.top {
+  background-color: red;
+  height: 30px;
+  margin-bottom: 25px;
+  display: flex;
+  justify-content: space-between;
+}
+.addButton {
+  width: 30px;
+  height: 30px;
+  background-color: blueviolet;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.search {
+  width: 430px;
+  background-color: azure;
+  display: flex;
+  justify-content: space-between;
+}
+.searchInput {
+  width: 380px;
+  background-color: black;
+}
+.todoForm {
+  background-color: cadetblue;
+  padding: 0 10px 0 10px;
+}
+</style>
+<script setup></script>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
+
+// 创建应用实例
+const app = createApp(App)
+
+// 注册所有图标组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+// 使用ElementPlus
+app.use(ElementPlus)
+
+// 挂载应用
+app.mount('#app')
